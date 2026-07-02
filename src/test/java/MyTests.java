c3a609c5d763a093820cd8fb01aaceebe5103525
Matvey4oo <atmanovm5@gmail.com>
2026-07-02 08:33:49 +0300

    AAAAAAAAAAAAAAA

---
+++ src/test/java/MyTests.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class MyTests {
+    @Test
+    public void myTest(){
+        System.out.println("MyTest");
+        assertEquals("Test","66");
+
+    }
+
+}
